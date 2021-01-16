6041579b5314d8bcd1b62442d873b8fbd57b1569
zhaolu <zhaolu16120284@163.com>
2021-01-16 12:13:15 +0800

    添加any与all BIF

--- bif_.py
+++ bif_.py
@@ -15,3 +15,10 @@ print(list(enumerate(a)))
 #map & filter
 print(list(map(lambda x: x * 2, [1,2,3,4,5])))
 print(list(filter(lambda x: x % 2 == 0, [1,2,3,4,5,6,7,8,9])))
+
+#all&any
+a = [0, False, "", 0, 0, 0]
+print(any(a)) #只有全部为False，才返回False
+
+a = [1, True, "a", 1, 1, 1]
+print(all(a)) #只有全为True，才返回True
