299b764edc23d34d03d7aef633b0c28f24a54cf8
zhaolu <zhaolu16120284@163.com>
2021-04-10 17:34:30 +0800

    数字字母转换，math计算

---
+++ math.py
@@ -0,0 +1,16 @@
+import math
+
+#绝对值
+a = -9
+print(abs(a))
+
+a = 9
+#平方、开方
+print(a ** 2)
+print(a ** 0.5)
+
+a = 9.125
+#ceil, floor, round
+print(math.ceil(a))
+print(math.floor(a))
+print(round(a, 2))
---
+++ str_.py
@@ -0,0 +1,6 @@
+#ord(), chr()
+a = 'A'
+print(ord(a))
+
+a = 65
+print(chr(a))
