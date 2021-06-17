d0a1f63f2e1f53e37613d7fc68ddd9649c2d1d72
zhaolu <zhaolu16120284@163.com>
2021-06-17 20:39:47 +0800

    any/all，强制类型转换

--- bif_.py
+++ bif_.py
@@ -19,6 +19,16 @@ print(list(filter(lambda x: x % 2 == 0, [1,2,3,4,5,6,7,8,9])))
 #all&any
 a = [0, False, "", 0, 0, 0]
 print(any(a)) #只有全部为False，才返回False
+#if any i == 0 for i in a
+any(i == 0 for i in a)
 
 a = [1, True, "a", 1, 1, 1]
 print(all(a)) #只有全为True，才返回True
+#if all i == 0 for i in a
+all(i == 0 for i in a)
+
+#强制类型转换
+int('10')
+int('110011', 2)
+str()
+bin()
